e877f70fe76a1a2bd5790c332d2004d5cf3a6561
Jahsie Ault <jahsieault@gmail.com>
2015-07-22 17:17:24 -0700

    Jahsie's style tweaks and moved one homework

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Super Awesome Homework Tester' });
 });
 
 module.exports = router;
